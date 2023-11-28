d26efdbbb28e0405046729e4ac00a59cf8558823
SelmanBilgen <112642878+SelmanBilgen@users.noreply.github.com>
2023-11-28 04:46:34 -0500

    Add files via upload
    Responsiveness + Form Validation Done

---
+++ components/Confirmation.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Confirmation({ values }) {
+  const today = new Date(values.date);
+  const correctedDate = new Date(today);
+  correctedDate.setDate(today.getDate() + 1);
+  return (
+    <div className="confirmation">
+      <div className="confirmation-container">
+        <br />
+        <h3>
+          {values.firstName} {values.lastName}
+        </h3>{" "}
+        <h3 style={{ color: "#f4ce14" }}>
+          Your reservation has been confirmed.
+        </h3>
+        <h3 style={{ color: "#f4ce14" }}>
+          A confirmation email has been sent to:
+        </h3>
+        <h3>{values.email}</h3>
+        <h3 style={{ color: "#f4ce14" }}>Your reservation is on:</h3>
+        <h3>
+          {new Date(correctedDate).toDateString()}
+          {" - "} {values.time}
+          {" - "} {"Party of "}
+          {values.party}
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default Confirmation;
---
+++ components/FindTable.js
@@ -0,0 +1,134 @@
+import React from "react";
+import "./Booking.css";
+
+function FindTable({
+  value,
+  handleChange,
+  //myDate,
+  availableTimes,
+  handleDateChange,
+  handleSubmit,
+}) {
+  const today = new Date(value.date);
+  const correctedDate = new Date(today);
+  correctedDate.setDate(today.getDate() + 1);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="reservations-container">
+        <div className="inputs-container1">
+          <div className="party-input">
+            <label>
+              Party
+              <input
+                name="party"
+                id="party"
+                type="number"
+                min={1}
+                max={12}
+                value={value.party}
+                onChange={handleChange}
+                className="reservations-party"
+                required={true}
+              />
+            </label>
+          </div>
+          <div className="date-input">
+            <label>
+              Date
+              <input
+                name="date"
+                id="date"
+                type="date"
+                value={value.date}
+                onChange={handleDateChange}
+                className="reservations-date"
+                min={new Date().toJSON().slice(0, 10)}
+                required={true}
+              />
+            </label>
+          </div>
+          <div className="time-input">
+            <label>
+              Time
+              <select
+                name="time"
+                className="reservations-time"
+                value={value.time}
+                onChange={handleChange}
+                required={true}
+              >
+                Time
+                {availableTimes.map((times) => (
+                  <option data-testid="time-option" key={times} value={times}>
+                    {times}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+        </div>
+        <div className="inputs-container2">
+          <label>
+            Occasion
+            <select
+              name="occasion"
+              className="select-occasion"
+              value={value.occasion}
+              onChange={handleChange}
+              required={true}
+            >
+              <option value="anniversary">Anniversary</option>
+              <option value="birthday">Birthday</option>
+              <option value="engagement">Engagement</option>
+              <option value="other">Other</option>
+            </select>
+          </label>
+          <label>
+            Seating
+            <select
+              name="seating"
+              className="select-seating"
+              value={value.seating}
+              onChange={handleChange}
+              required={true}
+            >
+              <option value="indoor">Indoor</option>
+              <option value="outdoor">Outdoor</option>
+            </select>
+          </label>
+        </div>
+        <h2>Your Reservation Summary</h2>
+        <p
+          className="reservation-summary"
+          style={{ fontSize: "1rem", marginBottom: "1rem" }}
+        >
+          {" - "}
+          {"Party of "}
+          {value.party}
+        </p>
+        <p style={{ textTransform: "capitalize", color: "white" }}>
+          {" - "}
+          {new Date(correctedDate).toDateString()}
+          <br />
+          <br />
+
+          {" - "}
+          {value.time}
+
+          <br />
+          <br />
+          {" - "}
+          {value.occasion}
+
+          <br />
+          <br />
+          {" - "}
+          {value.seating}
+        </p>
+      </div>
+    </form>
+  );
+}
+
+export default FindTable;
